07ee2f4d235b67c019c8919f031d38daf7784ef3
Estevan Rafael Marimon Arrieta <estevan.rafael07@hotmail.es>
2024-11-21 01:13:29 +0000

    Merged PR 4: primeros cambios
    primeros cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './presentation/pages/home/home.component';
+import { HomeComponent } from './features/home/pages/home/home.component';
+import { AboutUsComponent } from './features/about-us/pages/about-us/about-us.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'about', component: AboutUsComponent },
 ];
