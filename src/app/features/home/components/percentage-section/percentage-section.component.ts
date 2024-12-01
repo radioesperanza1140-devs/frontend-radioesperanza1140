b0fc3d28969fbfe649ed28fcd49cf299b07eabf1
estebanm21 <eteban087@gmail.com>
2024-12-01 17:59:55 -0500

    testimonios cambio de color cards

---
+++ src/app/features/home/components/percentage-section/percentage-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-percentage-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './percentage-section.component.html',
+  styleUrl: './percentage-section.component.scss'
+})
+export class PercentageSectionComponent {
+
+}
