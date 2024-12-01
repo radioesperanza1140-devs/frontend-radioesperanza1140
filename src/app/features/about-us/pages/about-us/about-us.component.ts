00695ab1f756e25258d7e1cee71ef031a94f7c3f
estebanm21 <eteban087@gmail.com>
2024-12-01 18:57:30 -0500

    about-us

--- src/app/features/about-us/pages/about-us/about-us.component.ts
+++ src/app/features/about-us/pages/about-us/about-us.component.ts
@@ -5,8 +5,18 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './about-us.component.html',
-  styleUrl: './about-us.component.scss'
+  styleUrl: './about-us.component.scss',
 })
 export class AboutUsComponent {
+  mision = '';
+  vision = '';
 
+  constructor(private __aboutUsService: AboutUsService) {}
+
+  ngOnInit() {
+    this.__aboutUsService.getData().subscribe((data: any) => {
+      (this.mision = data.data[0].mision), (this.vision = data.data[0].vision);
+      console.log(data);
+    });
+  }
 }
