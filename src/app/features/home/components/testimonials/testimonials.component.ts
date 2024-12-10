6f0e380aa91027cefb35b4ad5a87b33e8a509b29
estebanm21 <eteban087@gmail.com>
2024-12-10 16:30:51 -0500

    cambios donation component

--- src/app/features/home/components/testimonials/testimonials.component.ts
+++ src/app/features/home/components/testimonials/testimonials.component.ts
@@ -1,13 +1,47 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-testimonials',
   standalone: true,
   imports: [],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA], 
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './testimonials.component.html',
-  styleUrl: './testimonials.component.scss'
+  styleUrl: './testimonials.component.scss',
 })
 export class TestimonialsComponent {
+  public value = 0;
+  public windowWidth = window.innerWidth;
+  constructor() {}
 
+  ngOnInit(): void {}
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.windowWidth = window.innerWidth;
+  }
+
+  changeslidesPerView() {
+    if (this.windowWidth > 1266) {
+      this.value = 3;
+    } else if (this.windowWidth < 1266) {
+      this.value = 2;
+    }
+    if (this.windowWidth < 1100) {
+      this.value = 2;
+    }
+
+    if (this.windowWidth < 860) {
+      this.value = 1;
+    }
+
+    if (this.windowWidth < 690) {
+      this.value = 1;
+    }
+
+    if (this.windowWidth < 380) {
+      this.value = 1;
+    }
+
+    return this.value;
+  }
 }
